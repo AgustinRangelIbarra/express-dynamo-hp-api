70d25fa60b8c417a211484c3e734c499e6dfb75b
Agustin Rangel <agustinran.ib@gmail.com>
2022-02-15 22:14:38 -0600

    express, dynamo crud

---
+++ app.js
@@ -0,0 +1,77 @@
+const express = require("express");
+const {
+	getAllCharacters,
+	getCharacterById,
+	deleteCharacter,
+	addOrUpdateCharacter,
+} = require("./dynamo");
+const app = express();
+
+// parse data that comes from a post request
+app.use(express.json());
+
+app.get("/", (req, res) => {
+	res.send("Hello world");
+});
+
+app.get("/characters", async (req, res) => {
+	try {
+		const cahracters = await getAllCharacters();
+		res.json(cahracters);
+	} catch (error) {
+		console.log(error);
+		res.status(500).json({ err: "something went wrong" });
+	}
+});
+
+app.get("/characters/:id", async (req, res) => {
+	const id = req.params.id;
+	try {
+		const cahracters = await getCharacterById(id);
+		res.json(cahracters);
+	} catch (error) {
+		console.log(error);
+		res.status(500).json({ err: "something went wrong" });
+	}
+});
+
+app.post("/characters", async (req, res) => {
+	const character = req.body;
+	try {
+		const newCharacter = await addOrUpdateCharacter(character);
+		res.json(newCharacter);
+	} catch (error) {
+		console.log(error);
+		res.status(500).json({ err: "something went wrong" });
+	}
+});
+
+app.put("/character/:id", async (req, res) => {
+	const character = req.body;
+	const { id } = req.params;
+	character.id = id;
+	try {
+		const updatedCharacter = await addOrUpdateCharacter(character);
+		res.json(updatedCharacter);
+	} catch (error) {
+		console.log(error);
+		res.status(500).json({ err: "something went wrong" });
+	}
+});
+
+app.delete("/characters/:id", async (req, res) => {
+	const { id } = req.params;
+
+	try {
+		res.json(await deleteCharacter(id));
+	} catch (error) {
+		console.error(error);
+		res.status(500).json({ err: "Something went wrong" });
+	}
+});
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+	console.log(`listening on port ${port}`);
+});
